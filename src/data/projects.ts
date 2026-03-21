7e56ebcd4b53ca6a49264069640740593f5e0128
Wagner Brüggemann <wagner.bruggemann@gmail.com>
2026-03-21 14:46:24 -0300

    [main] feat: AI integration integrated into the projects portifolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,9 +3,58 @@ import SansysImageOne from '@/assets/SANSYS_1.png'
 import SansysImageTwo from '@/assets/SANSYS_2.png'
 import SansysImageThree from '@/assets/SANSYS_3.png'
 import SansysImageFour from '@/assets/SANSYS_4.png'
+import promptGeneratorImg from '@/assets/PROMPT_GENERATOR.png'
+import promptGeneratorImgOne from '@/assets/PROMPT_GENERATOR1.png'
+import promptGeneratorImgTwo from '@/assets/PROMPT_GENERATOR2.png'
+import promptGeneratorImgThree from '@/assets/PROMPT_GENERATOR3.png'
+import promptGeneratorImgFour from '@/assets/PROMPT_GENERATOR4.png'
+import promptGeneratorImgWhite from '@/assets/PROMPT_GENERATOR_white.png'
+import promptGeneratorImgOneWhite from '@/assets/PROMPT_GENERATOR1_white.png'
+import promptGeneratorImgTwoWhite from '@/assets/PROMPT_GENERATOR2_white.png'
+import promptGeneratorImgThreeWhite from '@/assets/PROMPT_GENERATOR3_white.png'
+import promptGeneratorImgFourWhite from '@/assets/PROMPT_GENERATOR4_white.png'
 import type { Project } from '@/types/Types'
 
 export const projects: Project[] = [
+  {
+    active: true,
+    opened: false,
+    mainImage: promptGeneratorImg,
+    company: 'Personal Project',
+    companyLink: '',
+    title: 'Prompt Generator',
+    imagesSectionTitle: 'public_images',
+    tags: ['Node', 'Python', 'AI Integration', 'Prompt Enginnering'],
+    items: [
+      {
+        title: 'prompt_generator_description_section',
+        sections: ['prompt_generator_description'],
+      },
+      {
+        title: 'prompt_generator_features_section',
+        sections: [
+          'prompt_generation',
+          'ai_agent_definition',
+          'prd_and_spring_planner',
+          'document_generator',
+          'content_enrichment',
+          'dual_interface',
+        ],
+      },
+    ],
+    images: [
+      promptGeneratorImg,
+      promptGeneratorImgWhite,
+      promptGeneratorImgOne,
+      promptGeneratorImgOneWhite,
+      promptGeneratorImgTwo,
+      promptGeneratorImgTwoWhite,
+      promptGeneratorImgThree,
+      promptGeneratorImgThreeWhite,
+      promptGeneratorImgFour,
+      promptGeneratorImgFourWhite,
+    ],
+  },
   {
     active: true,
     opened: false,
